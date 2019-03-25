114c4129eef502dc275baf7cc69a30da172acfdf
Vanbungkring <prasetyo.mailbox@gmail.com>
2019-03-25 10:28:35 +0700

    Initial Commit

---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Vanbungkring on 3/25/19.
+//  Copyright © 2019 netra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
